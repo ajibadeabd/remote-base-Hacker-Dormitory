77560b84f5add27a4e29bc10d56c945aa4fa7395
ajibadeabd <43596899+ajibadeabd@users.noreply.github.com>
2022-12-27 20:59:01 +0100

    completed the hacker dormitory test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ResidentsList from './Components/ResidentsList';
 import Search from './Components/Search';
 import Error from './Components/Error';
 import 'h8k-components';
 
-const title = "Hacker Dormitory";
+const title = "Tizeti";
 function App() {
+  const [resident, setResident] = useState([])
+  const [error, setError] = useState("")
+ 
   return (
     <div className="App">
-        <h8k-navbar header={title}></h8k-navbar>
+        <h8k-navbar header={title} ></h8k-navbar>
       <div className="layout-column justify-content-center align-items-center w-50 mx-auto">
-        <Search />
-        <Error/>
-        <ResidentsList/>
+        <Search setResident={setResident} setError={setError}/>
+        <Error error={error}/>
+        <ResidentsList resident={resident}/>
       </div>
     </div>
   );
